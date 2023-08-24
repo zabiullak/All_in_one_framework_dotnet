2663c24cabaa911850c3d2065fac85eda5f6d0ec
Mohamad Zabiulla <mohamad.khaja@qualitestgroup.com>
2023-08-24 17:42:23 +0530

    added playwright project

---
+++ UI_Automation_Playwright/Features/Weather.feature
@@ -0,0 +1,8 @@
+﻿Feature: Weather
+
+@LocalWeather
+Scenario: Check My Local Weather
+	Given i navigate to "https://www.bbc.co.uk/weather"
+	When i input the location "Bangalore, India"
+	And  click search
+	Then i see current weather for "Bangalore"
---
+++ UI_Automation_Playwright/Steps/Weather_Steps.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace UI_Automation_Playwright.Steps
+{
+    [Binding]
+    public sealed class Weather_Steps
+    {
+        private IPage _page;
+
+        public Weather_Steps(IPage page)
+        {
+            _page = page;
+        }
+        [Given(@"i navigate to ""(.*)""")]
+        public async Task GivenINavigateTo(string p0)
+        {
+            await _page.GotoAsync(p0);
+        }
+
+        [When(@"i input the location ""(.*)""")]
+        public async Task WhenIInputTheLocation(string p0)
+        {
+            await _page.FillAsync("//input[@placeholder='Enter a city']", p0);
+        }
+
+        [When(@"click search")]
+        public async Task WhenClickSearch()
+        {
+            await Task.WhenAll(_page.PressAsync("//input[@placeholder='Enter a city']", "Enter"));
+        }
+
+        [Then(@"i see current weather for ""(.*)""")]
+        public async Task ThenISeeCurrentWeatherFor(string p0)
+        {
+            string expected = await _page.InnerTextAsync("//*[@id='wr-location-name-id']");
+            expected.Should().BeEquivalentTo(p0);
+        }
+    }
+}
