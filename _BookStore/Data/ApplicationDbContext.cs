1bc793292160e417cb820b3345f052b224241026
Mohamad Zabiulla <mohamad.khaja@qualitestgroup.com>
2023-10-28 20:47:14 +0530

    created database|connection string|dataannotation

---
+++ _BookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _BookStore.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option) 
+        {
+            
+        }
+    }
+}
