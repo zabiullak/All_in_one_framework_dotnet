2443b6793a77dc7d252172cb6f057506a508be75
Mohamad Zabiulla <mohamad.khaja@qualitestgroup.com>
2023-10-29 23:47:24 +0530

    added Edit|Delete, toaster notification

--- _BookStore/Controllers/CategoryController.cs
+++ _BookStore/Controllers/CategoryController.cs
@@ -21,5 +21,82 @@ namespace _BookStore.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "test is an invalid value");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id==0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult Delete_POST(int? id)
+        {
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted successfully!";
+            return RedirectToAction("Index");
+        }
     }
 }
--- _BookStore/Models/Category.cs
+++ _BookStore/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace _BookStore.Models
 {
@@ -6,8 +7,14 @@ namespace _BookStore.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; }
+        
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage ="Display Order must be btw 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
